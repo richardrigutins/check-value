a0cc9d4ebb079a5b78db080313fca0ba81d9f568
richardrigutins <47950599+richardrigutins@users.noreply.github.com>
2026-03-29 03:29:23 +0200

    chore: migrate to ESM with Rollup

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  clearMocks: true,
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  preset: 'ts-jest',
+  resolver: 'ts-jest-resolver',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        useESM: true,
+      },
+    ],
+  },
+  verbose: true,
+};
